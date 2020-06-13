3c250de15d7c68834e9ea40168723144438c855a
abdullahamir52 <abdullahamir52@gmail.com>
2020-06-14 02:28:00 +0600

    2nd Push

--- Course 2 (R Programming) Code 2 Console Input and Evaluation, Data types, Subsetting.r
+++ Course 2 (R Programming) Code 2 Console Input and Evaluation, Data types, Subsetting.r
@@ -71,3 +71,61 @@ a1 <- cbind(x, y)
 a1
 a2 <- rbind(x, y)
 a2
+
+
+
+
+# R Data types: Factors
+# Factors can be ordered(lecturer, professor) or un-ordered(Male,female)
+x <- factor(c("yes", "yes", "no", "yes", "no"))
+x
+table(x)
+unclass(x)
+
+# To change the order of the levels
+# Since no comes before yes, alphabetically, the primary level is no. 
+# you can change it using the level function 
+x <- factor(c("yes", "yes", "no", "yes", "no"),
+              levels = c("yes", "no"))
+x
+
+
+
+# Missing values
+x <- c(1, 2, NA, 10, 3)
+is.na(x)
+is.nan(x)
+x <- c(1, 2, NaN, NA, 4)
+is.na(x)
+is.nan(x)
+# A NaN value is also NA but the converse is not true
+
+
+
+# Data Frames
+x <- data.frame(foo = 1:4, bar = c(T, T, F, F))
+x
+nrow(x)
+ncol(x)
+
+
+
+
+# Names
+
+# Naming objects
+x <- 1:3
+names(x)
+names(x) <- c("foo", "bar", "norf")
+x
+names(x)
+
+# Naming lists
+x <- list(a = 1, b = 2, c = 3)
+x
+
+# Naming matrices
+m <- matrix(1:4, nrow = 2, ncol = 2)
+dimnames(m) <- list(c("a", "b"), c("c", "d"))
+m
+
