0d98aae35d52204405db52f790b2a2640ce27b16
abdullahamir52 <abdullahamir52@gmail.com>
2020-07-30 00:34:02 +0600

    Created a file that will have some essential commands to use in Rstudio

---
+++ R studio codes.r
@@ -0,0 +1,29 @@
+# To clear the environment in rstudio
+Ctrl + Shift + F10
+
+# To see the working directory
+getwd()
+
+# To see the files in the directory
+dir()
+
+# To clear the console
+Ctrl + L
+
+# To remove an object (factor, list, vector)
+rm(object_name)
+
+# To write a comment
+Ctrl + Shift + C
+
+# To run a selected section of lines
+Ctrl + Enter
+
+# To delete a file
+unlink("some_file.csv")
+
+# To delete another file
+file.remove("some_other_file.csv")
+
+# To delete a directory -- must add recursive = TRUE
+unlink("some_directory", recursive = TRUE)
