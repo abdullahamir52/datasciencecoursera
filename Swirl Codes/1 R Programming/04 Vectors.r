ef66bb13184e5e8c26472b0a3a6ec450f2476dfa
abdullahamir52 <abdullahamir52@gmail.com>
2020-08-06 10:15:24 +0600

    changed writing styles of some output

--- Swirl Codes/1 R Programming/04 Vectors.r
+++ Swirl Codes/1 R Programming/04 Vectors.r
@@ -97,12 +97,14 @@ paste(1:3, c("X", "Y", "Z"), sep = "")
 # predefined variable in R containing a character vector of all 26 letters in the
 # English alphabet.
 paste(LETTERS, 1:4, sep = "-")
-# [1] "A-1" "B-2" "C-3" "D-4" "E-1" "F-2" "G-3" "H-4" "I-1" "J-2" "K-3" "L-4" "M-1" "N-2"
-# [15] "O-3" "P-4" "Q-1" "R-2" "S-3" "T-4" "U-1" "V-2" "W-3" "X-4" "Y-1" "Z-2"
+# [1] "A-1" "B-2" "C-3" "D-4" "E-1" "F-2" "G-3" "H-4" "I-1" "J-2" 
+# "K-3" "L-4" "M-1" "N-2""O-3" "P-4" "Q-1" "R-2" "S-3" "T-4" "U-1" "V-2" 
+# "W-3" "X-4" "Y-1" "Z-2"
 
 paste(1:4, LETTERS, sep = "-")
-# [1] "1-A" "2-B" "3-C" "4-D" "1-E" "2-F" "3-G" "4-H" "1-I" "2-J" "3-K" "4-L" "1-M" "2-N"
-# [15] "3-O" "4-P" "1-Q" "2-R" "3-S" "4-T" "1-U" "2-V" "3-W" "4-X" "1-Y" "2-Z"
+# [1] "1-A" "2-B" "3-C" "4-D" "1-E" "2-F" "3-G" "4-H" "1-I" "2-J" "3-K" 
+# "4-L" "1-M" "2-N" "3-O" "4-P" "1-Q" "2-R" "3-S" "4-T" "1-U" "2-V" 
+# "3-W" "4-X" "1-Y" "2-Z"
 
 # Since the character vector LETTERS is longer than the numeric vector 1:4, R simply
 # recycles, or repeats, 1:4 until it matches the length of LETTERS.
