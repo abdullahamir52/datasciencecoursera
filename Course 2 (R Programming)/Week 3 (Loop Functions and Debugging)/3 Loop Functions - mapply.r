3c252eec2ef5b444349acb06bdaadd8e7edf4a01
abdullahamir52 <abdullahamir52@gmail.com>
2020-08-14 17:44:26 +0600

    Done with the 3rd video of week 3

--- Course 2 (R Programming)/Week 3 (Loop Functions and Debugging)/3 Loop Functions - mapply.r
+++ Course 2 (R Programming)/Week 3 (Loop Functions and Debugging)/3 Loop Functions - mapply.r
@@ -4,6 +4,14 @@
 # mapply is a multivariate apply of sorts which applies a function in parallel 
 # over a set of arguments.
 
+# For instance, we can only use a single list as an argument in lapply and 
+# sapply. What if we want to use multiple lists/vectors and apply a function
+# to those objects? We can use a for loop where the for loop will index the 
+# elements of each of the different lists. Or we can use the mapply. 
+
+# Condition: Number of args the function takes has to be at least as many 
+# as the number of lists within mapply.
+
 str(mapply)
 # function (FUN, ..., MoreArgs = NULL, SIMPLIFY = TRUE, USE.NAMES = TRUE) 
 
@@ -16,9 +24,23 @@ str(mapply)
 # mapply (contd.)
 # The following is tedious to type
 list(rep(1, 4), rep(2, 3), rep(3, 2), rep(4, 1))
+# Output
+# [[1]]
+# [1] 1 1 1 1
+# 
+# [[2]]
+# [1] 2 2 2
+# 
+# [[3]]
+# [1] 3 3
+# 
+# [[4]]
+# [1] 4
+
 
 # Instead we can do
 mapply(rep, 1:4, 4:1)
+# Output
 # [[1]]
 # [1] 1 1 1 1
 # 
@@ -33,25 +55,31 @@ mapply(rep, 1:4, 4:1)
 
 
 
-# Vectorizing a Function
-# -----------------------
+# Vectorizing a Function (generates some random normal noise)
+# ------------------------------------------------------------
 noise <- function(n, mean, sd) 
   {
     rnorm(n, mean, sd)
-}
+  }
+# n     = number of obs
+# mean  = value of the mean
+# sd    = value of the standard deviation
 
 noise(5, 1, 2)
 # Output
-# [1] 2.958112 1.658940 4.759271 5.085798 3.657153
+# [1] -1.5809945 -0.8601625 -0.9740305  2.7608355 -2.2720245
 # Or
-# [1]  1.320754 -5.506440  0.340139  1.914180 -1.608208
+# [1] -5.0106501  1.0511306  0.6218588 -0.8802973  0.6745357
 
-noise(1:5, 1:5, 2)
+noise(1:5, 1:5, 2) # not works with a vector of arguments. see below
 # Output
 # [1]  0.56066271 -0.08009486  3.38589957  2.88952750 5.54998178
 # Or
 # [1] 1.074713 6.361667 2.840830 6.752590 8.150633
 
+# What I want to have is one random normal with mean 1, two random normals 
+# with mean 2, three random normals with mean 3, etc, and then five random 
+# normals with mean 5.
 
 
 # Instant Vectorization
@@ -60,19 +88,19 @@ noise(1:5, 1:5, 2)
 mapply(noise, 1:5, 1:5, 2)
 # Output
 # [[1]]
-# [1] -1.582347
-
+# [1] 2.264323
+# 
 # [[2]]
-# [1]  3.812759 -3.591923
-
+# [1] -0.05351147 -0.31719546
+# 
 # [[3]]
-# [1] 3.467121 2.461928 1.682910
-
+# [1] -1.151527  1.020959  4.165155
+# 
 # [[4]]
-# [1] 4.126692 4.108954 4.494019 4.248071
-
+# [1] 3.907000 1.369469 3.833469 3.492890
+# 
 # [[5]]
-# [1] 3.929487 5.516965 7.775418 6.334341 7.994099
+# [1] 3.751370 6.303556 6.741944 3.799147 5.635567
 
 
 # Which is the same as
@@ -81,16 +109,16 @@ list(noise(1, 1, 2), noise(2, 2, 2),
      noise(5, 5, 2))
 # Output
 # [[1]]
-# [1] 0.4502392
-
+# [1] 4.021776
+# 
 # [[2]]
-# [1] 3.166152 3.006717
-
+# [1] -0.9477724  1.7838555
+# 
 # [[3]]
-# [1] 1.513505 1.366245 6.740567
-
+# [1] 1.929739 3.777856 2.048152
+# 
 # [[4]]
-# [1] 2.345494 5.901425 3.467655 1.801836
-
+# [1] 3.807770 7.024827 4.638591 5.441814
+# 
 # [[5]]
-# [1] 1.931565 2.587380 1.099949 3.231920 6.946812
+# [1] 7.628596 4.933261 4.325802 4.440892 5.996562
