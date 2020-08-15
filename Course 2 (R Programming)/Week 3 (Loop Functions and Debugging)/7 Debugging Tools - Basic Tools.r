a260b712ae0380869259aabbc4a568996108ee60
abdullahamir52 <abdullahamir52@gmail.com>
2020-08-15 17:10:48 +0600

    Done with 7th video of week 3

--- Course 2 (R Programming)/Week 3 (Loop Functions and Debugging)/7 Debugging Tools - Basic Tools.r
+++ Course 2 (R Programming)/Week 3 (Loop Functions and Debugging)/7 Debugging Tools - Basic Tools.r
@@ -17,6 +17,29 @@
 # 5. recover: allows you to modify the error behavior so that you can browse the 
 # function call stack
 
+
+# Traceback just tells you how many function calls you're in and 
+# where the error occurred
+
+# So you give debug function another function as an argument, and it flags 
+# that function for debug mode. Debug means anytime you execute that function, 
+# anytime, anywhere even if another function calls it, it will, it will halt, 
+# it will suspend execution of the function at the first line, and then you 
+# can, in what's called a browser.
+
+# The browser function's relativity to debug in the sense that you can stick 
+# call to the browser function anywhere in your code, and when that line of 
+# code gets hit the execution of the function will suspend. And then you can 
+# go line by line from there.
+
+# Recover is an error handler function which means that any time you encounter 
+# an error anywhere in a function, rather than getting the console back the R 
+# interpreter will stop the execution of that function right where the error 
+# occurred, and will kind of freeze it there. And then it will print out the
+# function call stack, so you'll see how far deep you're in, and then you can 
+# go, you can kind of jump around to the different function calls to see, 
+# and to look around. 
+
 # These are interactive tools specifically designed to allow you to pick 
 # through a function. There’s also the more blunt technique of inserting 
 # print/cat statements in the function.
