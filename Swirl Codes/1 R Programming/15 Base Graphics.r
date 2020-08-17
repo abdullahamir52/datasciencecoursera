e19196371675a90a4aa3103ddf3e0bceeacdbba6
abdullahamir52 <abdullahamir52@gmail.com>
2020-08-17 14:00:21 +0600

    Started swirl code. R programming. Chapter 15 Base graphics.

---
+++ Swirl Codes/1 R Programming/15 Base Graphics.r
@@ -0,0 +1,138 @@
+# -----------------------------------------------------
+# Chapter 15: Base Graphics  
+
+# One of the greatest strengths of R, relative to other programming languages, 
+# is the ease with which we can create publication-quality graphics. In this 
+# lesson, you'll learn about base graphics in R.
+
+# We do not cover the more advanced portions of graphics in R in this lesson.
+# These include lattice, ggplot2 and ggvis.
+
+# There is a school of thought that this approach is backwards, that we should
+# teach ggplot2 first. See
+# http://varianceexplained.org/r/teach_ggplot2_to_beginners/ for an outline of
+# this view.
+
+# Load the included data frame cars with data(cars).
+
+data(cars)
+
+# To fix ideas, we will work with simple data frames. Our main goal is to
+# introduce various plotting functions and their arguments. All the output 
+# would look more interesting with larger, more complex data sets.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
