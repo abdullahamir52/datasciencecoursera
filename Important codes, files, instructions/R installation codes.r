2c71f9840cca9f98547500af0e53a32ab3fb74f0
abdullahamir52 <abdullahamir52@gmail.com>
2021-09-30 04:17:57 -0400

    added the contour function package installation code

--- Important codes, files, instructions/R installation codes.r
+++ Important codes, files, instructions/R installation codes.r
@@ -14,7 +14,7 @@ install.packages("learnr")
 install.packages("rmarkdown")
 install.packages("dplyr")
 install.packages("data.table")
-
+install.packages("ContourFunctions")
 
 
 # Updating R/RStudio/Tidyverse
@@ -22,31 +22,21 @@ install.packages("data.table")
 install.packages("installr")
 library(installr)
 updateR()
-# From within RStudio, go to Help > Check for Updates 
+# From within RStudio, go to Help > Check for Updates
 # to install newer version of RStudio (if available, optional).
-
-
-
-# to use rmarkdown to make pdf, you need to install latex package: tinytex
-install.packages('tinytex')
-tinytex::install_tinytex()
-# to uninstall TinyTeX, run tinytex::uninstall_tinytex() 
-tinytex:::is_tinytex() 
-# install at the very last. takes too long to install
-
 
 
 # Learn R using SWIRL
 install.packages("swirl")
 # If you've installed swirl in the past make sure having version 2.2.21 or later.
-# You can check this with: 
+# You can check this with:
 packageVersion("swirl")
 # Every time you want to use swirl, you need to first load the package.
 library("swirl")
-# swirl offers a variety of interactive courses, 
+# swirl offers a variety of interactive courses,
 # install_course("Course Name Here")
-# but for our purposes, you want the one called R Programming. 
-# Type the following from the R prompt to install this course: 
+# but for our purposes, you want the one called R Programming.
+# Type the following from the R prompt to install this course:
 install_from_swirl("R Programming")
 # go to https://github.com/swirldev/swirl_courses#swirl-courses
 # Start swirl and complete the lessons
@@ -60,11 +50,18 @@ install_course("Regression Models")
 install_course("Getting and Cleaning Data")
 install_course("Statistical Inference")
 install_course("Advanced R Programming")
-
 # Creating a path for Rtools
 writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
 # then restart R (Ctrl + shift + F10)
 Sys.which("make")
-#                                         make 
-# "C:\\PROGRA~1\\rtools40\\usr\\bin\\make.exe" 
+#                                         make
+# "C:\\PROGRA~1\\rtools40\\usr\\bin\\make.exe"
 install.packages("jsonlite", type = "source")
+
+
+# to use rmarkdown to make pdf, you need to install latex package: tinytex
+install.packages('tinytex')
+tinytex::install_tinytex()
+# to uninstall TinyTeX, run tinytex::uninstall_tinytex()
+tinytex:::is_tinytex()
+# install at the very last. takes too long to install
